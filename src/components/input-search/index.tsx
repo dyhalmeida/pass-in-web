368e588c25427a335cc7f5913940673595272421
dyhalmeida <dyhalmeida@gmail.com>
2024-04-02 10:26:23 -0300

    feat: add input search component

---
+++ src/components/input-search/index.tsx
@@ -0,0 +1,17 @@
+import { Search } from "@/lib/icons";
+
+export const InputSearch = () => {
+  return (
+    <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+      <label htmlFor="search">
+        <Search className="size-4 text-emerald-300" />
+      </label>
+      <input
+        id="search"
+        type="text"
+        placeholder="Buscar participante..."
+        className="bg-transparent flex-1 outline-none"
+      />
+    </div>
+  );
+};
