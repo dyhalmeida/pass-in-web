c593620bff17717f381e819b62e08841060da1da
dyhalmeida <dyhalmeida@gmail.com>
2024-04-05 20:57:16 -0300

    feat: add props to input search component

--- src/components/input-search/index.tsx
+++ src/components/input-search/index.tsx
@@ -1,6 +1,9 @@
 import { Search } from "@/lib/icons";
+import { ComponentProps } from "react";
 
-export const InputSearch = () => {
+interface IInputSearch extends ComponentProps<"input"> {}
+
+export const InputSearch = (props: IInputSearch) => {
   return (
     <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
       <label htmlFor="search">
@@ -10,6 +13,7 @@ export const InputSearch = () => {
         id="search"
         type="text"
         placeholder="Buscar participante..."
+        {...props}
         className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
       />
     </div>
